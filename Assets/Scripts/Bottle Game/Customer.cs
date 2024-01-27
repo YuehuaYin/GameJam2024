2533766cacf7d3f5829f551cab417a01a61faa0c
bobtie658 <40718295+bobtie658@users.noreply.github.com>
2024-01-27 18:04:00 +0000

    images imanges glasss filkiung

--- Assets/Scripts/Bottle Game/Customer.cs
+++ Assets/Scripts/Bottle Game/Customer.cs
@@ -6,19 +6,23 @@ public class Customer : MonoBehaviour
 {
     [SerializeField]
     private MeshFilter meshFilter;
+    private Mesh mesh;
+
+    public int targetLiquid = 30;
+    public int currectLiquid;
     
     void Start()
     {
-        Mesh mesh = new Mesh();
-
+        mesh = new Mesh();
+        
         Vector3[] verticies = new Vector3[4];
         Vector2[] uv = new Vector2[4];
         int[] triangles = new int[6];
 
         verticies[0] = new Vector3(-0.283f, -0.49f);
-        verticies[1] = new Vector3(-0.45f, 0.26f);
-        verticies[2] = new Vector3(0.45f, 0.26f);
-        verticies[3] = new Vector3(00.283f, -0.49f);
+        verticies[1] = new Vector3(-0.283f, -0.49f);
+        verticies[2] = new Vector3(0.283f, -0.49f);
+        verticies[3] = new Vector3(0.283f, -0.49f);
 
         uv[0] = new Vector2(0, 0);
         uv[1] = new Vector2(0, 1);
@@ -36,12 +40,46 @@ public class Customer : MonoBehaviour
         mesh.vertices = verticies;
         mesh.uv = uv;
         mesh.triangles = triangles;
+
+        meshFilter.mesh = mesh;
+
+        StartCoroutine(Test());
+    }
+
+    private void addLiquid()
+    {
+        float x = (currectLiquid / targetLiquid) * (0.45f - 0.283f);
+        float y = (currectLiquid / targetLiquid) *(0.26f + 0.49f);
+
+        Vector3[] vertices = new Vector3[4];
         
+        vertices[0] = new Vector3(-0.283f, -0.49f);
+        vertices[1] = new Vector3(-x, y);
+        vertices[2] = new Vector3(x, y);
+        vertices[3] = new Vector3(0.283f, -0.49f);
+
+        mesh.vertices = vertices;
+        
+        meshFilter.mesh = mesh;
+        
+        Debug.Log(x + "," + y);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator Test()
+    {
+        while (currectLiquid < targetLiquid)
+        {
+            currectLiquid++;
+            addLiquid();
+            Debug.Log(currectLiquid);
+
+            yield return null;
+        }
     }
 }
