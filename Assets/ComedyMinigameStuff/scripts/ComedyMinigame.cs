1112a029d7502edfeff0f8e5b5987da23f241466
Yuehua Yin <yy.yin02@gmail.com>
2024-01-27 11:59:56 +0000

    start comedy minigame

---
+++ Assets/ComedyMinigameStuff/scripts/ComedyMinigame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComedyMinigame : MonoBehaviour
+{
+
+    List<string> emojibank = new List<string> { "cheese", "devil", "eggpplant", "ghost", "nerd", "sadcat", "shark", "skull", "smile", "think" };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
