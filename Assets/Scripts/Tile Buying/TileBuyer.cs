4ad9afa1cc4d00990b68ac74ec328bad2b907ddf
Krankenstein74 <157914311+Krankenstein74@users.noreply.github.com>
2024-01-28 01:33:41 +0000

    Generate any no of Tiles, can buy tiles, fixed bug where you could buy already owned tiles,
    I know hot to designate certain tiles to be toilets doors etc

--- Assets/Scripts/Tile Buying/TileBuyer.cs
+++ Assets/Scripts/Tile Buying/TileBuyer.cs
@@ -15,23 +15,82 @@ public class TileBuyer : MonoBehaviour
     public Tile rightWall;
     public Tile unbuilt;
 
+    public int money = 5;
+
     public Tilemap tm;
 
     private Dictionary<Vector2Int, Tile> tileCoords;
 
+    private Dictionary<Vector2Int, bool> isOccupied = new Dictionary<Vector2Int, bool>();
+
     //Add more of these to set up the initial state for each level
     private void level1Start() {
         //How the level starts
-        tm.SetTile(new Vector3Int(0,0),unbuilt);
-        tm.SetTile(new Vector3Int(0,-1),unbuilt);
-        tm.SetTile(new Vector3Int(-1,0),unbuilt);
-        tm.SetTile(new Vector3Int(-1,-1),unbuilt);
+        // tm.SetTile(new Vector3Int(0,0),unbuilt);
+        // tm.SetTile(new Vector3Int(0,-1),unbuilt);
+        // tm.SetTile(new Vector3Int(-1,0),unbuilt);
+        // tm.SetTile(new Vector3Int(-1,-1),unbuilt);
+        // //tm.SetTile(new Vector3Int(-1,-2),unbuilt);
 
-        //The things you can buy
-        tileCoords.Add(new Vector2Int(0,0),corner);
-        tileCoords.Add(new Vector2Int(0,-1),rightWall);
-        tileCoords.Add(new Vector2Int(-1,0),leftWall);
-        tileCoords.Add(new Vector2Int(-1,-1),empty);     
+        // //The things you can buy
+        // tileCoords.Add(new Vector2Int(0,0),corner);
+        // tileCoords.Add(new Vector2Int(0,-1),toilets);
+        // tileCoords.Add(new Vector2Int(-1,0),leftWall);
+        // tileCoords.Add(new Vector2Int(-1,-1),empty);     
+        buildXY(2,2);
+        //tileCoords.Add(new Vector2Int(-1,-2),empty);  
+
+        //Building stuff to start with
+
+        buildInitialTile(new Vector2Int(0,0));
+        buildInitialTile(new Vector2Int(0,-1));
+    }
+    public void level2Start(){
+        buildXY(3,3);
+
+    }
+    public void level3Start(){
+        buildXY(4,4);
+
+    }
+    public void level4Start(){
+        buildXY(5,5);
+
+    }
+    public void level5Start(){
+        buildXY(6,6);
+
+    }
+    public void level6Start(){
+        buildXY(7,7);
+
+    }
+
+    public void buildXY(int x, int y){
+        
+        
+        
+        for (int i = 0; i < x ; i++){
+            for (int j = 0; j < y ; j++){
+                tm.SetTile(new Vector3Int(-i,-j),unbuilt);
+            }
+        }
+        for (int i = 0; i < x ; i++){
+            for (int j = 0; j < y ; j++){
+                if (i == 0 && j ==0) {
+                    tileCoords.Add(new Vector2Int(-i,-j),corner);
+                }else if (i == 0){
+                    tileCoords.Add(new Vector2Int(-i,-j),rightWall);
+                }else if (j == 0){
+                    tileCoords.Add(new Vector2Int(-i,-j),leftWall);
+                } else{
+                    tileCoords.Add(new Vector2Int(-i,-j),empty);
+                } 
+                isOccupied.Add(new Vector2Int(-i,-j),false);
+            }
+            
+        }
+
     }
 
     //Replace the values to alter the prices of tiles for each level
@@ -47,8 +106,23 @@ public class TileBuyer : MonoBehaviour
         }
     }
     
-    public void buildTile(Vector2Int pos) {
+    public void buildInitialTile(Vector2Int pos) {
         tm.SetTile(convVector(pos),tileCoords[pos]);
+    }
+    public void buildTile(Vector2Int pos) {
+        Vector3Int v = new Vector3Int(pos.x,pos.y);
+        if (isOccupied.TryGetValue(pos, out bool isItOccupoed)){
+
+        
+
+
+        if(money > 0 && !isItOccupoed){
+            money = money -1;
+            
+            tm.SetTile(convVector(pos),tileCoords[pos]);
+            isOccupied[(pos)] = true;
+        }
+        }
     }
 
 
@@ -60,7 +134,32 @@ public class TileBuyer : MonoBehaviour
     void Start()
     {
         tileCoords = new Dictionary<Vector2Int,Tile> {};
+        int i = GameManager.level;
+        i = 1;
+        switch (i){
+        case 1:
         level1Start();
+        break;
+        case 2:
+        level2Start();
+        break;
+        case 3:
+        level3Start();
+        break;
+        case 4:
+        level4Start();
+        break;
+        case 5:
+        level5Start();
+        break;
+        case 6:
+        level6Start();
+        break;
+
+
+        
+        }
+        
     }
 
     // Update is called once per frame
