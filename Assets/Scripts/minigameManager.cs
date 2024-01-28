9e4068dde2bef37099a96b86b2a2993cdf4c22cf
Kai Davis <kd2g20@soton.ac.uk>
2024-01-28 14:15:59 +0000

    minigame timer

--- Assets/Scripts/minigameManager.cs
+++ Assets/Scripts/minigameManager.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class minigameManager : MonoBehaviour
 {
+    public timerBar timerbar;
 
     [SerializeField] public GameObject comedyGame;
     [SerializeField] public GameObject barGame;
@@ -13,16 +15,47 @@ public class minigameManager : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI moneyText;
 
+    [SerializeField] private float timeLimit = 90;
+    [SerializeField] private float timer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        timerbar.updateTimerBar(timer, timeLimit);
+        if (timer >= timeLimit)
+        {
+            switch (GameManager.level)
+            {
+                case 1:
+                    SceneManager.LoadScene("Level1");
+                    break;
+                case 2:
+                    SceneManager.LoadScene("Level2");
+                    break;
+                case 3:
+                    SceneManager.LoadScene("Level3");
+                    break;
+                case 4:
+                    SceneManager.LoadScene("Level4");
+                    break;
+                case 5:
+                    SceneManager.LoadScene("Level5");
+                    break;
+                case 6:
+                    SceneManager.LoadScene("Level6");
+                    break;
+                default:
+                    SceneManager.LoadScene("Level1");
+                    break;
+            }
+        }
     }
 
     public void disableComedy()
