3edce83e1e4e67d3a62ea0c638a3732fb9ab567c
bobtie658 <40718295+bobtie658@users.noreply.github.com>
2024-01-28 15:07:38 +0000

    Added background, updated credits

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource source1;
+    [SerializeField] private AudioSource source2;
+
+    [SerializeField] private AudioClip basement;
+    [SerializeField] private AudioClip club;
+    [SerializeField] private AudioClip arena;
+    [SerializeField] private AudioClip space;
+    [SerializeField] private AudioClip evil;
+
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+            SceneManager.activeSceneChanged += SceneChange;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        source1.clip = basement;
+        source1.Play();
+    }
+
+    private void SceneChange(Scene start, Scene next)
+    {
+        
+    }
+}
