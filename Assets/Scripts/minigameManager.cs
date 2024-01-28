3cdd41d174fbdf77a17971293bc678578953554a
Kai Davis <kd2g20@soton.ac.uk>
2024-01-28 14:27:39 +0000

    changed level scene names in minigame manager

--- Assets/Scripts/minigameManager.cs
+++ Assets/Scripts/minigameManager.cs
@@ -34,22 +34,22 @@ public class minigameManager : MonoBehaviour
             switch (GameManager.level)
             {
                 case 1:
-                    SceneManager.LoadScene("Level1");
+                    SceneManager.LoadScene("Level 1");
                     break;
                 case 2:
-                    SceneManager.LoadScene("Level2");
+                    SceneManager.LoadScene("Level 2");
                     break;
                 case 3:
-                    SceneManager.LoadScene("Level3");
+                    SceneManager.LoadScene("Level 3");
                     break;
                 case 4:
-                    SceneManager.LoadScene("Level4");
+                    SceneManager.LoadScene("Level 4");
                     break;
                 case 5:
-                    SceneManager.LoadScene("Level5");
+                    SceneManager.LoadScene("Level 5");
                     break;
                 case 6:
-                    SceneManager.LoadScene("Level6");
+                    SceneManager.LoadScene("Level 6");
                     break;
                 default:
                     SceneManager.LoadScene("Level1");
